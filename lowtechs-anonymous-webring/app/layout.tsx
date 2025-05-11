9b86e28f5f0dab3f272a0ca2e41e48a72b154ee4
Timothy Beck <dev@timothybeck.com>
2025-05-10 23:01:09 -0600

    added vt323 font and next event date function

--- lowtechs-anonymous-webring/app/layout.tsx
+++ lowtechs-anonymous-webring/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local';
 
+const vt323 = localFont({ src: './fonts/VT323-Regular.ttf' })
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${vt323.className} `}>
         {children}
       </body>
     </html>
--- lowtechs-anonymous-webring/app/page.tsx
+++ lowtechs-anonymous-webring/app/page.tsx
@@ -1,95 +1,55 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+export default function Home() {
+
+    const getNextAlaEvent = (today = new Date()) => {
+        let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth()+1, 0);
+        //Find offset from end of month to find last thursday
+        let offset = 0;
+        switch(lastDayOfMonth.getDay()){
+            case 0:
+                offset = -3;
+                break;
+            case 1:
+                offset = -4;
+                break;
+            case 2:
+                offset = -5;
+                break;
+            case 3:
+                offset = -6;
+                break;
+            case 4:
+                offset = 0;
+                break;
+            case 5:
+                offset = -1;
+                break;
+            case 6:
+                offset = -2;
+                break;
+        }
+        //Find Last Thrusday of this month
+        let lastThursday = new Date(lastDayOfMonth.getFullYear(), lastDayOfMonth.getMonth()+1, offset, 18);
+        //If last thursday is after today, find next month's event
+        if(lastThursday < today){
+            return getNextAlaEvent(new Date(today.getFullYear(), today.getMonth()+2, 1));
+        }
+        //convert date to string and return shortened string
+        let str = lastThursday.toString();
+        let arr = str.split(' ').slice(0,5);
+        return arr.join(' ');
+    };
+
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <h1 className={styles.titleFont}>ALA!</h1>
+                <p className={styles.tagLine}>Nerds Seeking Sobriety From Tech Illiteracy</p>
+                <p className={styles.dateText}>Next Event: {getNextAlaEvent().toString()}</p>
+            </main>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
