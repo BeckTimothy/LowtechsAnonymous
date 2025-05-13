42586c5b005ebf345a4cf270a112e6e4044b72ed
Timothy Beck <dev@timothybeck.com>
2025-05-13 12:06:40 -0600

    created redirection middleware

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 Smol project for a cluster of nerds.
 
 Site TODO List:
-- [ ] Discord redirect **/discord**
-- [ ] Eventbrite redirect **/event**
-- [ ] webring redirect **/next**
-- [ ] webring redirect **/last**
+- [x] Discord redirect **/discord**
+- [x] Eventbrite redirect **/event**
+- [x] webring redirect **/next**
+- [x] webring redirect **/last**
 - [x] webring crawler/health maintainer scripts **api/webring**
 - [ ] build SSR-proof crawler/health maintainer scripts w/ puppeteer or similar
 - [ ] wayback machine crawler/ auto archiver **/archive**
---
+++ lowtechs-anonymous-webring/middleware.ts
@@ -0,0 +1,70 @@
+import { NextResponse, NextRequest } from 'next/server'
+import { get } from '@vercel/edge-config'
+import webringData from './app/_data/webring.json'
+import {sort} from "next/dist/build/webpack/loaders/css-loader/src/utils";
+
+type RedirectEntry = {
+    destination: string
+    permanent: boolean
+}
+
+export async function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname.startsWith('/discord')) {
+        return NextResponse.redirect(new URL('https://discord.gg/GYjrTjBEYf'))
+    }
+    if (request.nextUrl.pathname.startsWith('/event')) {
+        return NextResponse.redirect(new URL('https://www.eventbrite.com/e/albuquerque-lowtechs-anonymous-tickets-798974804147'))
+    }
+    if (request.nextUrl.pathname.startsWith('/next/')) {
+        //get siteName from pathname
+        let site = request.nextUrl.pathname.substring(6).toLowerCase()
+        let sortedArr = Array.from(webringData);
+        for(let i=0;i<sortedArr.length;i++){
+            if(sortedArr[0].siteName !== site){
+                let siteData = sortedArr.shift();
+                if(siteData)sortedArr.push(siteData)
+            }else{
+                i=sortedArr.length;
+            }
+
+        }
+        for(let i=1;i<sortedArr.length;i++){
+            if(sortedArr[i].isLive && sortedArr[i].isValid){
+                return NextResponse.redirect(new URL('https://' + sortedArr[i].siteName))
+            }
+        }
+        //redirect to next site
+        return NextResponse.rewrite(new URL('/', request.url))
+    }
+    if (request.nextUrl.pathname.startsWith('/prev/')) {
+        //get siteName from pathname
+        let site = request.nextUrl.pathname.substring(6).toLowerCase()
+        let sortedArr = Array.from(webringData);
+        for(let i=0;i<sortedArr.length;i++){
+            if(sortedArr[0].siteName !== site){
+                let siteData = sortedArr.shift();
+                if(siteData)sortedArr.push(siteData)
+            }else{
+                sortedArr = sortedArr.reverse();
+                i=sortedArr.length;
+            }
+
+        }
+
+        for(let i=0;i<sortedArr.length;i++){
+            if(sortedArr[i].isLive && sortedArr[i].isValid){
+                return NextResponse.redirect(new URL('https://' + sortedArr[i].siteName))
+            }
+        }
+
+        //redirect to previous site or lander. not sure.
+        return NextResponse.rewrite(new URL('/', request.url))
+    }
+
+    // No redirect found, continue without redirecting
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/next/:path*', '/prev/:path*', '/discord/:path*', '/event/:path*'],
+}
